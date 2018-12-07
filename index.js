87ed11437b6e089331a24adf367fd8402320cb2f
Kathryn Dineen <krdineen@charter.net>
2018-12-07 04:07:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ console.error("HALP")
 console.error("HALP")
 console.log("i would be a logger")
 console.warn("is this right")
+var word
+word = 'bird'
+console.log(word)
+word = 'dog'
+console.log(word)
+window.word
+var word
+var eric
+eric = 'kate'
+console.log(eric)
+var a = 5
+var b = 2
